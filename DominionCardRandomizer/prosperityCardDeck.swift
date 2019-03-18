c5820a51f69ed5b597fb803eb4d69ef01ef8cae5
Dan Morse <djmthedev@gmail.com>
2019-03-17 19:39:15 -0700

    Adding Prosperity cards

---
+++ DominionCardRandomizer/prosperityCardDeck.swift
@@ -0,0 +1,25 @@
+//
+//  prosperityCardDeck.swift
+//  DominionCardRandomizer
+//
+//  Created by morse on 3/17/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+/// Lines 10 - 33 set up data for Prosperity deck.
+let bank = Card(name: "Bank", cost: 7, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 1, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let bishop = Card(name: "Bishop", cost: 4, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 1, trashCardUpTo: 1, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let contraband = Card(name: "Contraband", cost: 5, plusCard: 0, plusAction: 0, plusBuy: 1, plusCoin: 3, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let city = Card(name: "City", cost: 5, plusCard: 1, plusAction: 2, plusBuy: 0, plusCoin: 0, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let countingHouse = Card(name: "Counting House", cost: 5  , plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 1, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let expand = Card(name: "Expand", cost: 7, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 0, trashCardUpTo: 1, gainCard: 1, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let forge = Card(name: "Forge", cost: 7, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 0, trashCardUpTo: 5, gainCard: 5, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let goons = Card(name: "Goons", cost: 6, plusCard: 0, plusAction: 0, plusBuy: 1, plusCoin: 2, trashCardUpTo: 0, gainCard: 0, isAttack: true, isDefense: false, fromDeck: "Prosperity")
+let grandMarket = Card(name: "Grand Market", cost: 6, plusCard: 1, plusAction: 1, plusBuy: 1, plusCoin: 2, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let hoard = Card(name: "Hoard", cost: 6, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 2, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let kingsCourt = Card(name: "King's Court", cost: 7, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 0, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let Loan = Card(name: "Loan", cost: 3, plusCard: 1, plusAction: 0, plusBuy: 0, plusCoin: 1, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let mint = Card(name: "Mint", cost: 5, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 0, trashCardUpTo: 0, gainCard: 1, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let monument = Card(name: "Monument", cost: 4, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 2, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
+let mountebank = Card(name: "Mountebank", cost: 5, plusCard: 0, plusAction: 0, plusBuy: 0, plusCoin: 2, trashCardUpTo: 0, gainCard: 0, isAttack: true, isDefense: false, fromDeck: "Prosperity")
+let peddler = Card(name: "Peddler", cost: 8, plusCard: 1, plusAction: 1, plusBuy: 0, plusCoin: 1, trashCardUpTo: 0, gainCard: 0, isAttack: false, isDefense: false, fromDeck: "Prosperity")
