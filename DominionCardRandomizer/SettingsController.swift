b6fdaddb566cf607be8f58bfd3a78a4d0b15b678
Dan Morse <djmthedev@gmail.com>
2019-03-22 12:00:07 -0700

    created settings screen and dismiss buttons. still need to add settings switches

---
+++ DominionCardRandomizer/SettingsController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsController.swift
+//  DominionCardRandomizer
+//
+//  Created by morse on 3/22/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissSettings(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
